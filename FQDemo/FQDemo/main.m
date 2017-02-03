63199bf8415a2c047999f69b42ca0123ebccc1ec
qiwancong <qiwancong@sina.com>
2017-02-03 10:06:43 +0800

    Just demo

---
+++ FQDemo/FQDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FQDemo
+//
+//  Created by flower on 2017/2/3.
+//  Copyright © 2017年 flower. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
